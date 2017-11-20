5f92216b6e7c02636e0573a295713b54fe7cd36a
Igor Pisarev <pisarevi@janelia.hhmi.org>
2017-11-20 17:31:43 -0500

    remove unnecessary import

--- src/main/java/org/janelia/saalfeldlab/n5/bdv/googlecloud/GoogleCloudBrowseHandler.java
+++ src/main/java/org/janelia/saalfeldlab/n5/bdv/googlecloud/GoogleCloudBrowseHandler.java
@@ -8,7 +8,6 @@ import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
 
