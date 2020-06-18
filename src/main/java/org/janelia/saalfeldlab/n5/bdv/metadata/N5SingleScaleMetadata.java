29143702dbf3deadfa8703326e72ad643b2d0400
Igor Pisarev <pisarevi@janelia.hhmi.org>
2020-06-18 16:09:05 -0400

    Add dataset path to N5SingleScaleMetadata

--- src/main/java/org/janelia/saalfeldlab/n5/bdv/metadata/N5SingleScaleMetadata.java
+++ src/main/java/org/janelia/saalfeldlab/n5/bdv/metadata/N5SingleScaleMetadata.java
@@ -4,10 +4,13 @@ import net.imglib2.realtransform.AffineTransform3D;
 
 public class N5SingleScaleMetadata implements N5Metadata {
 
+    public final String path;
+
     public final AffineTransform3D transform;
 
-    public N5SingleScaleMetadata(final AffineTransform3D transform)
+    public N5SingleScaleMetadata(final String path, final AffineTransform3D transform)
     {
+        this.path = path;
         this.transform = transform;
     }
 }
